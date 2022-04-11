c131685311fd6e910d8a76620c7b0a1f1714c322
ErnestoCarballo <ernestowcarballo12@gmail.com>
2022-04-10 23:02:09 -0700

    second commit

--- DaikichiController.java
+++ DaikichiController.java
@@ -1,5 +1,6 @@
 package com.ernesto.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +11,28 @@ public class DaikichiController {
 	public String index() {
 		return "Welcome!";
 	}
-	
+
 	@RequestMapping("/today")
 	public String luck() {
 		return "Today you will find luck in all your endeavors!";
 	}
-	
+
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+	@RequestMapping("/travel/{location}")
+	public String travel(@PathVariable("location") String location) {
+		return "Congratulations! You will soon travel to " + location;
+	}
+
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") Integer number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
